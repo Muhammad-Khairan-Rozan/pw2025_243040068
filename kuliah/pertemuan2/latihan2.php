ea5b71b63aed476f4c3d196bbd9d37433923e62e
Muhammad-Khairan-Rozan <mkrozan@gnail.com>
2025-02-17 11:38:11 +0700

    tes

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "Muhammad Khairan Rozan";
+$email = "mkrozan@gmail.com";
+$pekerjaan = "mahasiswa";
+$jurusan = "teknik informatika";
+$universitas = "Universitas Pasundan";
+echo "<h1>$nama</h1>";
+echo "<p>$email</p>";
+echo "Halo, nama saya $nama, saya adalah $pekerjaan di jurusan $jurusan $universitas";
+?>
